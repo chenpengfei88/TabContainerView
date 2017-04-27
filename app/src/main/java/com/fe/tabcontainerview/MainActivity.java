1992515b0e7ea2148b58a67de8f1e566e14dec1a
陈鹏飞 <pengfei.chen@weimob.com>
2017-04-27 13:48:02 +0800

    udpate example

--- app/src/main/java/com/fe/tabcontainerview/MainActivity.java
+++ app/src/main/java/com/fe/tabcontainerview/MainActivity.java
@@ -4,8 +4,7 @@ import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import com.fe.library.TabContainerView;
-import com.fe.library.listener.OnTabSelectedListener;
-import com.fe.library.widget.Tab;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,15 +19,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initView() {
+        Fragment[] fragments = new Fragment[] {new MainFragment(), new WorkFragment(), new AppFragment(), new MineFragment()};
         tabContainerView = (TabContainerView) findViewById(R.id.tab_containerview_main);
-
-        tabContainerView.setAdapter(new MainTabContainerAdapter(getSupportFragmentManager(),
-                new Fragment[] {new MainFragment(), new WorkFragment(), new AppFragment(), new MineFragment()}));
-
-        tabContainerView.setOnTabSelectedListener(new OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(Tab tab) {
-            }
-        });
+        tabContainerView.setAdapter(new MainTabContainerAdapter(getSupportFragmentManager(), fragments));
     }
 }
